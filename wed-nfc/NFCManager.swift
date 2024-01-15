af940c6f16fe98da4095b29e8a2f992054a779be
kyoya1123 <kyoryu32@gmail.com>
2024-01-15 17:49:29 +0900

    update textpayload

--- wed-nfc/NFCManager.swift
+++ wed-nfc/NFCManager.swift
@@ -4,27 +4,31 @@ import CoreNFC
 class NFCManager: NSObject {
     
     static let shared = NFCManager()
-
+    
     private var session: NFCNDEFReaderSession!
     private var isWriting = false
     private var ndefMessage: NFCNDEFMessage!
     private var writeHandler: (() -> Void)?
     private var readHandler: ((String?) -> Void)!
-
+    
     func write(text: String, handler: (() -> Void)? = nil) {
         writeHandler = handler
         isWriting = true
-        let textPayload = NFCNDEFPayload.wellKnownTypeTextPayload(string: text, locale: .init(identifier: "ja_JP"))!
+        let textPayload = NFCNDEFPayload(
+            format: NFCTypeNameFormat.nfcWellKnown,
+            type: "T".data(using: .utf8)!,
+            identifier: Data(),
+            payload: text.data(using: .utf8)!)
         ndefMessage = NFCNDEFMessage(records: [textPayload])
         startSession()
     }
-
+    
     func read(handler: @escaping ((String?) -> Void)) {
         readHandler = handler
         isWriting = false
         startSession()
     }
-
+    
     private func startSession() {
         guard NFCNDEFReaderSession.readingAvailable else {
             return
@@ -36,16 +40,16 @@ class NFCManager: NSObject {
 }
 
 extension NFCManager: NFCNDEFReaderSessionDelegate {
-
+    
     func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
     }
-
+    
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
     }
-
+    
     func readerSessionDidBecomeActive(_ session: NFCNDEFReaderSession) {
     }
-
+    
     func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
         let tag = tags.first!
         session.connect(to: tag) { error in
@@ -58,7 +62,7 @@ extension NFCManager: NFCNDEFReaderSessionDelegate {
             }
         }
     }
-
+    
     private func write(tag: NFCNDEFTag, session: NFCNDEFReaderSession) {
         tag.writeNDEF(self.ndefMessage) { [unowned self] _ in
             session.alertMessage = "書き込み完了"
@@ -68,13 +72,16 @@ extension NFCManager: NFCNDEFReaderSessionDelegate {
             }
         }
     }
-
+    
     private func read(tag: NFCNDEFTag, session: NFCNDEFReaderSession) {
         tag.readNDEF { [unowned self] message, _ in
             session.alertMessage = "読み込み完了"
             session.invalidate()
-            let text = message?.records.first?.wellKnownTypeTextPayload().0
             DispatchQueue.main.async {
+                guard let payload = message?.records.first?.payload, let text = String(data: payload, encoding: .utf8) else {
+                    self.readHandler(nil)
+                    return
+                }
                 self.readHandler(text)
             }
         }
